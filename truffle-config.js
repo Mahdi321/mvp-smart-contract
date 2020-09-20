e1b41b8dbc945fae3103f96e1abbd1eb0858b74d
Mahdi321 <mahg361@gmail.com>
2020-09-21 02:13:01 +0330

    up

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,11 @@ module.exports = {
       development: {
         host: "localhost",
         port: 8545,
-        gas: 6721975,
+            
+        gas: 5721975000000,
+        gasPrice: 1000000000,
+        //gasLimit: 10000000000,
+        //blockLimit: 10000000000,
         network_id: "*" // Match any network id
       }
     },
